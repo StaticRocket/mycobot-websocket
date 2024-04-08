f198ce0297cdfd23934acd66edd41b50643e9a48
Randolph Sapp <rs@ti.com>
2024-04-08 17:16:22 -0500

    server: attempt to use some real gcode
    There are some things I'm not familiar enough to address right now.
    Backing away from this process for now. The internal axes definitions
    are a little lost on me. They seem to position the head around a give
    point. This breaks the internal kinematic solver if you attempt to keep
    these values the same and change the X, Y, or Z positions.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import asyncio
 from websockets.server import serve
 import pymycobot
 from pymycobot.genre import Angle
+from pymycobot.genre import Coord
 
 # Configurables
 SERVER = "0.0.0.0"
@@ -14,49 +15,83 @@ MC = pymycobot.MyCobot("/dev/ttyS3", pymycobot.PI_BAUD)
 
 # Globals
 CMD_LOCK = asyncio.Lock()
+GLOBAL_FEED = 50
 
 
-def on():
+async def on():
     """
     Ensure the device is powered on
     """
-    if MC.is_power_on():
-        print("Already on")
-    else:
+    async with CMD_LOCK:
         MC.power_on()
+        return MC.get_error_information()
 
 
-def off():
+async def off():
     """
     Ensure the device is powered off
     """
-    if not MC.is_power_on():
-        print("Already off")
-    else:
+    async with CMD_LOCK:
         MC.power_off()
+        return MC.get_error_information()
 
 
-def parse_cmd(cmd):
+async def home():
+    """
+    Try to home the device
+    """
+    async with CMD_LOCK:
+        MC.send_angles([0] * 7, 50)
+        return MC.get_error_information()
+
+
+async def linear_move(cmd_args):
+    """
+    Parse the extra parameters for the linear move command
+    """
+    feed_rate = GLOBAL_FEED
+    axies = ["X", "Y", "Z", "A", "B", "C", "D"]
+    updated = [0, 0, 0, 0, 0, 0, 0]
+    async with CMD_LOCK:
+        coords = MC.get_coords()
+    for arg in cmd_args:
+        if arg[0] == "F":
+            feed_rate = int(arg[1:])
+        elif arg[0] in axies:
+            index = axies.index(arg[0])
+            coords[index] = int(arg[1:])
+        else:
+            print("Unparsed argument:", arg)
+    async with CMD_LOCK:
+        MC.send_coord(coords, feed_rate)
+        return MC.get_error_information()
+    return 1
+
+
+async def parse_cmd(cmd):
     """
     Unravel command
     """
-    cmd_arr = cmd.split()
-    if len(cmd_arr) == 3:
-        try:
-            joint = int(cmd_arr[0])
-            position = int(cmd_arr[1])
-            speed = int(cmd_arr[2])
-        except:
-            print("Failed to parse command: ", cmd)
-            return 1
+    if not cmd:
+        return 1
 
-        try:
-            MC.send_angle(joint, position, speed)
-            return 0
-        except Exception as e:
-            print("Exception: ", e)
+    ret = 0
+    cmd_arr = cmd.upper().split()
+    if cmd_arr[0] == ";":
+        pass
+    elif cmd_arr[0] == "G1":
+        ret = await linear_move(cmd_arr[1:])
+    elif cmd_arr[0] == "G30":
+        ret = await home()
+    elif cmd_arr[0] == "M80":
+        ret = await on()
+    elif cmd_arr[0] == "M81":
+        ret = await off()
+    else:
+        print("Not implemented:", cmd)
+        ret = 1
 
-    return 1
+    return ret
 
 
 async def handler(websocket):
@@ -64,18 +99,13 @@ async def handler(websocket):
     Listen and respond to incoming messages
     """
     async for message in websocket:
-        async with CMD_LOCK:
-            parse_cmd(message)
+        await websocket.send(str(await parse_cmd(message)))
 
 
 async def main():
     """
-    Setup the device and start the websocket server
+    Start the websocket server
     """
-    async with CMD_LOCK:
-        on()
-        print("Zeroing out")
-        MC.send_angles([0, 0, 0, 0, 0, 0], 50)
     async with serve(handler, SERVER, PORT):
         await asyncio.Future()
 
